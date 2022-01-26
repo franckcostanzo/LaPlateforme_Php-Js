3d1bc109c92fca2fc14d228e8a1bcc916a7d659d
Franck Costanzo <costanzofranck@gmail.com>
2022-01-26 11:06:36 +0100

    debut memory

---
+++ LaPlateforme_/projects/memory/elements/footer.php
@@ -0,0 +1,10 @@
+<footer>
+    <nav class="navbar bg-dark w-10 fixed-bottom">
+        <!-- Navbar content -->
+        <div class="container-fluid d-flex flex-column align-items-center">
+            <a href="https://github.com/franckcostanzo/cours-php/tree/main/LaPlateforme_/projects/livreOr">
+                <i class="fab fa-github-square h1 text-secondary"></i>
+            </a>                
+        </div>
+    </nav>
+</footer>
