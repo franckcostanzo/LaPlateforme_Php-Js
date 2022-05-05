e590202bb30c774a72778b962b47fcaba575e0d6
franck-costanzo <franck.costanzo@laplateforme.io>
2022-05-05 15:20:15 +0200

    debut symfony 6

--- LaPlateforme_/ProjectPoolPhP/BlogMVC/index.php
+++ LaPlateforme_/ProjectPoolPhP/BlogMVC/index.php
@@ -3,6 +3,7 @@
     require_once 'Model/View.php';
     if( isset($_GET['url']) )
     {
+        //faire un try catch pour les url qui n'existe pas et faire une page 404
         $view = New View($_GET['url']);
         $view->display();
     }
